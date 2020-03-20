0043fe8a8b7e72b37f1ede1849163b00c638be59
Constantinoi <pescaalma@gmail.com>
2020-03-19 20:25:13 -0400

    Mapeamento das Rotas

--- src/api/todo/todo.js
+++ src/api/todo/todo.js
@@ -1,7 +1,7 @@
 const restful = require('node-restful')
 const mongoose = restful.mongoose
 
-const todoSchema = new mongoose.todoSchema({
+const todoSchema = new mongoose.Schema({
     description:{ //descricao da tarefa obrigatoria
         type:String,
         required:true
---
+++ src/config/routes.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+module.exports = function(server){
+
+    //API Routes
+    const router = express.Router()
+    server.use('/api', router)
+
+    //TODO Routes
+    const todoServices = require('../api/todo/todoServices')
+    todoServices.register(router, '/todos')//usar todos os metodos to do get e demais
+
+    
+
+}
